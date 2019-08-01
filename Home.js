a400f452e6d4edca4fcdc9e3625372352dd0a577
adibest <ahmadjayadibya@gmail.com>
2019-08-02 00:37:29 +0700

    section 6

--- Home.js
+++ Home.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import Horizontal from './components/Horizontal';
+import {StyleSheet, Text, View, ScrollView} from 'react-native';
+import Header from './app/components/Header';
+import Mid from './app/components/Mid';
+import Body from './app/components/Body';
 
 export default class Home extends React.Component {
 	render() {
 		return(
-			<View style={styles.container}>
-				<Horizontal />
-			</View>
+			<ScrollView>
+				<View style={styles.container}>
+					<Header />
+					<Mid />
+					<Body />
+				</View>
+			</ScrollView>
 		);
 	}
 }
@@ -15,7 +21,8 @@ export default class Home extends React.Component {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		backgroundColor: '#fff',
 		alignItems: 'center',
-		justifyContent: 'center',  
+		justifyContent: 'center',
 	},
 });
