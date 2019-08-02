99e21b191e481bef22156d5961c1eabf9460abb8
adibest <ahmadjayadibya@gmail.com>
2019-08-02 16:49:56 +0700

    section 7

--- Home.js
+++ Home.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import Horizontal from './components/Horizontal';
+import {StyleSheet, Text, View, ScrollView} from 'react-native';
+import Navbar from './components/Navbar';
+import Tabbar from './components/Tabbar';
+import Body from './components/Body';
+
+import data from './components/search.json';
 
 export default class Home extends React.Component {
 	render() {
 		return(
 			<View style={styles.container}>
-				<Horizontal />
+				<Navbar />
+				<ScrollView>
+				<Body video={data.items[0]} />
+				<Body video={data.items[1]} />
+				<Body video={data.items[2]} />
+				<Body video={data.items[3]} />
+				<Body video={data.items[4]} />
+				</ScrollView>
+				<Tabbar />
 			</View>
 		);
 	}
@@ -15,7 +27,6 @@ export default class Home extends React.Component {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',  
+		backgroundColor: '#fff',
 	},
 });
